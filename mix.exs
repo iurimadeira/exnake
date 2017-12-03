1c3ea9c5f0c283adb90bc82d400b782dd9c2cb66
Iuri Madeira <iurimadeira@gmail.com>
2017-12-03 00:50:15 -0200

    Updates Elixir and Phoenix

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exnake.Mixfile do
   def project do
     [app: :exnake,
      version: "0.0.1",
-     elixir: "~> 1.4",
+     elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
@@ -30,7 +30,7 @@ defmodule Exnake.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.4"},
+    [{:phoenix, "~> 1.3"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
